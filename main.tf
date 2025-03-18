5dd3a12cc1edcd770662ac24ab6c1c86dab37d28
Gerson <gersonrodriguessantos8@gmail.com>
2025-03-18 01:20:48 -0300

    refactor: adjust and add kubeflow configurations

--- main.tf
+++ main.tf
@@ -162,6 +162,67 @@ resource "argocd_application" "dex" {
   ]
 }
 
+resource "argocd_application" "kubeflow-configs" {
+  metadata {
+    name      = var.destination_cluster != "in-cluster" ? "kubeflow-configs-${var.destination_cluster}" : "kubeflow-configs"
+    namespace = var.argocd_namespace
+    labels = merge({
+      "application" = "kubeflow-configs"
+      "cluster"     = var.destination_cluster
+    }, var.argocd_labels)
+  }
+
+  timeouts {
+    create = "15m"
+    delete = "15m"
+  }
+
+  wait = var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? false : true
+
+  spec {
+    project = var.argocd_project == null ? argocd_project.this[0].metadata.0.name : var.argocd_project
+
+    source {
+      repo_url        = var.project_source_repo
+      path            = "charts/kubeflow/common/roles-namespaces"
+      target_revision = var.target_revision
+    }
+
+    destination {
+      name      = var.destination_cluster
+      namespace = var.namespace
+    }
+
+    sync_policy {
+      dynamic "automated" {
+        for_each = toset(var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? [] : [var.app_autosync])
+        content {
+          prune       = automated.value.prune
+          self_heal   = automated.value.self_heal
+          allow_empty = automated.value.allow_empty
+        }
+      }
+
+      retry {
+        backoff {
+          duration     = "20s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+        limit = "5"
+      }
+
+      sync_options = [
+        "CreateNamespace=true",
+      ]
+    }
+  }
+
+  depends_on = [
+    resource.argocd_application.oauth2-proxy,
+  ]
+}
+
 resource "argocd_application" "central-dashboard" {
   metadata {
     name      = var.destination_cluster != "in-cluster" ? "central-dashboard-${var.destination_cluster}" : "central-dashboard"
@@ -219,7 +280,7 @@ resource "argocd_application" "central-dashboard" {
   }
 
   depends_on = [
-    resource.argocd_application.dex,
+    resource.argocd_application.kubeflow-configs,
   ]
 }
 
@@ -227,6 +288,7 @@ resource "null_resource" "this" {
   depends_on = [
     resource.argocd_application.oauth2-proxy,
     resource.argocd_application.dex,
+    resource.argocd_application.kubeflow-configs,
     resource.argocd_application.central-dashboard,
   ]
 }
